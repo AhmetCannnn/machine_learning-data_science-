8842f5024ea93348928e3d49ea344dfb2b87147d
Ahmet Can Çıtak <140341889+AhmetCannnn@users.noreply.github.com>
2023-10-25 13:12:54 +0300

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -14,41 +14,47 @@ import pandas as pd
 # veri yukleme
 veriler = pd.read_csv('maaslar.csv')
 
+#data frame dilimleme (slice)
 x = veriler.iloc[:,1:2]
 y = veriler.iloc[:,2:]
+
+#NumPY dizi (array) dönüşümü
 X = x.values
 Y = y.values
 
 
-#linear regression
+#linear regression (doğrusal model oluşturma)
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X,Y)
 
+
+#polynomial regression 
+#doğrusal olmayan (nonlinear) model oluşturma
+#2. derceden polinom
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+x_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly,y)
+
+#4. dereceden polinom
+poly_reg3 = PolynomialFeatures(degree = 4)
+x_poly3 = poly_reg3.fit_transform(X)
+lin_reg3 = LinearRegression()
+lin_reg3.fit(x_poly3,y)
+
+# Görselleştirme
 plt.scatter(X,Y,color='red')
 plt.plot(x,lin_reg.predict(X), color = 'blue')
 plt.show()
 
-
-#polynomial regression
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
-x_poly = poly_reg.fit_transform(X)
-print(x_poly)
-lin_reg2 = LinearRegression()
-lin_reg2.fit(x_poly,y)
 plt.scatter(X,Y,color = 'red')
 plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)), color = 'blue')
 plt.show()
 
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
-x_poly = poly_reg.fit_transform(X)
-print(x_poly)
-lin_reg2 = LinearRegression()
-lin_reg2.fit(x_poly,y)
 plt.scatter(X,Y,color = 'red')
-plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.plot(X,lin_reg3.predict(poly_reg3.fit_transform(X)), color = 'blue')
 plt.show()
 
 #tahminler
@@ -58,12 +64,6 @@ print(lin_reg.predict([[6.6]]))
 
 print(lin_reg2.predict(poly_reg.fit_transform([[6.6]])))
 print(lin_reg2.predict(poly_reg.fit_transform([[11]])))
-
-
-
-
-
-
 
 
 
